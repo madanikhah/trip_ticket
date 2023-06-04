9121434b779ff1cd423547bc183401d983a49cc5
madanikhah <45796591+madanikhah@users.noreply.github.com>
2023-06-04 17:30:27 +0330

    Update sign_script.js

--- scripts/sign_script.js
+++ scripts/sign_script.js
@@ -1,1 +1,13 @@
+const signinIcon = document.getElementById('signin-icon');
+const backgroundOverlay = document.getElementById('background-overlay');
+const signinModal = document.getElementById('signin-modal');
 
+signinIcon.addEventListener('click', () => {
+  backgroundOverlay.style.display = 'block';
+  signinModal.style.display = 'block';
+});
+
+backgroundOverlay.addEventListener('click', () => {
+  backgroundOverlay.style.display = 'none';
+  signinModal.style.display = 'none';
+});
